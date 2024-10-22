cac5e49bb1769fc932619c211fb520610a856cd0
student-a210 <student-a210@PC-A210-7>
2024-10-22 13:28:26 +0500

    new commit

--- Pages/Partner/Item.xaml.cs
+++ Pages/Partner/Item.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using Master_Pol.Classes.Context;
 using Master_Pol.Classes.Model;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Master_Pol.Pages.Partner
 {
